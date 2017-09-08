1b8268c3603fca4979d18e3d864b99d99829f819
Кирилл <kirylanoshko@gmail.com>
2017-09-08 12:17:28 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->app->get('stats', [
+$router->get('stats', [
 	'as' => 'profile', 'uses' => 'ExampleController@showProfile'
 ]);
