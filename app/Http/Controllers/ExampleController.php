6c0dbefc9f3514c1e396b91c2da72701d3803376
Кирилл <kirylanoshko@gmail.com>
2017-09-08 13:02:27 +0300

    last fix

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -19,7 +19,9 @@ class ExampleController extends Controller
 
 		$response = [];
 		$response['vk'] = file_get_contents( 'http://api.vk.com/method/groups.getById?gid=78242229&fields=members_count');
+		$response['vk'] = json_decode($response['vk'])->response[0]->members_count;
 		$response['youtube'] = file_get_contents( 'https://www.googleapis.com/youtube/v3/channels?key=AIzaSyCVdq0LYftjXscOS8JK0q37M9uJ2KZkegQ&part=statistics&id=UCLXo7UDZvByw2ixzpQCufnA');
+		$response['youtube'] = json_decode($response['youtube'])->items[0]->statistics->subscriberCount;
 		return $response;
 	}
 
